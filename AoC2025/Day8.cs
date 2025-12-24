24dfb3b1820a1cf8092cf55fdc26663d2976450d
dawesome <michael.dawe@gmail.com>
2025-12-23 22:32:18 -0800

    Fixed the distance calculations, at least

--- AoC2025/Day8.cs
+++ AoC2025/Day8.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Numerics;
-
-namespace AoC2025;
+﻿namespace AoC2025;
 
 public class Pair
 {
@@ -15,135 +12,298 @@ public class Pair
     }
 }
 
+public class JunctionBox
+{
+    public double x;
+    public double y;
+    public double z;
+
+    public JunctionBox(double xCoord, double yCoord, double zCoord)
+    {
+        x = xCoord;
+        y = yCoord;
+        z = zCoord;
+    }
+    
+    public double Distance(JunctionBox other)
+    {
+        return Math.Sqrt(Math.Pow(x - other.x, 2) + Math.Pow(y - other.y, 2) + Math.Pow(z - other.z, 2));
+    }
+}
+
+public class JunctionPair
+{
+    public int ReferenceJunctionIndex;
+    public int ClosestJunctionIndex;
+    public double DistanceFromReferenceToClosestJunction;
+
+    public JunctionPair()
+    {
+        ReferenceJunctionIndex = -1;
+        ClosestJunctionIndex = -1;
+        DistanceFromReferenceToClosestJunction = double.MaxValue;
+    }
+    
+    public JunctionPair(int reference, int closest, double distance)
+    {
+        ReferenceJunctionIndex = reference;
+        ClosestJunctionIndex = closest;
+        DistanceFromReferenceToClosestJunction = distance;
+    }
+}
+
+class JunctionPairComparer : IEqualityComparer<JunctionPair>
+{
+    public bool Equals(JunctionPair x, JunctionPair y)
+    {
+
+        if (x.DistanceFromReferenceToClosestJunction == y.DistanceFromReferenceToClosestJunction)
+        {
+            if (x.ReferenceJunctionIndex == y.ReferenceJunctionIndex &&
+                x.ClosestJunctionIndex   == y.ClosestJunctionIndex)
+            {
+                return true;
+            } 
+            else if (x.ReferenceJunctionIndex == y.ClosestJunctionIndex &&
+                     x.ClosestJunctionIndex   == y.ReferenceJunctionIndex)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    public int GetHashCode(JunctionPair obj)
+    {
+        return obj.ReferenceJunctionIndex.GetHashCode() ^ obj.ClosestJunctionIndex.GetHashCode();
+    }
+}
+
+public class JunctionPairSorter: IComparer<JunctionPair>
+{
+    public int Compare(JunctionPair? x, JunctionPair? y)
+    {
+        return x.DistanceFromReferenceToClosestJunction.CompareTo(y.DistanceFromReferenceToClosestJunction);
+    }    
+}
+
+public class CircuitListSorter : IComparer<List<int>>
+{
+    public int Compare(List<int> x, List<int> y)
+    {
+        return y.Count.CompareTo(x.Count);
+    }
+}
+
 public class Day8
 {
-    public Vector3[] junctionBoxes;
-    public long[] closestJunctionIndexArray;
-    public float[] distanceToClosestJunctionArray;
-    public SortedList<float, Tuple<long, long>> distanceToJunctionsList = new();
+    public JunctionBox[] JunctionBoxes;
+    public int[] ClosestJunctionIndexArray;
+    public double[] DistanceToClosestJunctionArray;
+    public List<JunctionPair> JunctionDistanceList = new();
     
 
     public void PrintJunctionBoxes()
     {
-        foreach (Vector3 vector in junctionBoxes)
+        foreach (JunctionBox vector in JunctionBoxes)
         {
-            Console.WriteLine(vector);
+            Console.WriteLine( "[" + vector.x + ", " + vector.y + ", " + vector.z + "]");
         }
+    }
+
+    public void PrintCircuits(List<List<int>> circuits)
+    {
+        circuits.Sort(new CircuitListSorter());
+        
+        Console.Write("[ ");
+        foreach (List<int> circuit in circuits)
+        {
+            circuit.Sort();
+            Console.Write("[ ");
+            foreach (int i in circuit)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.Write("], ");
+        }
+        Console.WriteLine("]");
     }
     
     public void ParseLines(IEnumerable<string> lines)
     {
-        junctionBoxes = new Vector3[lines.Count()];
+        JunctionBoxes = new JunctionBox[lines.Count()];
         for (int count = 0; count < lines.Count(); ++count)
         {
             var coordinates = lines.ElementAt(count).Split(',');
-            junctionBoxes[count] = new Vector3(long.Parse(coordinates[0]), long.Parse(coordinates[1]), long.Parse(coordinates[2]));
+            JunctionBoxes[count] = new JunctionBox(double.Parse(coordinates[0]), double.Parse(coordinates[1]), double.Parse(coordinates[2]));
         }
     }
 
-    public long[] CalculateDistancesBetweenJunctionBoxes()
+    public int[] CalculateDistancesBetweenJunctionBoxes()
     {
-        long closestDistance = long.MaxValue;
-        closestJunctionIndexArray = new long[junctionBoxes.Length];
-        distanceToClosestJunctionArray = new float[junctionBoxes.Length];
+        // Build the list of pair distances
+        ClosestJunctionIndexArray = new int[JunctionBoxes.Length];
+        DistanceToClosestJunctionArray = new double[JunctionBoxes.Length];
         
-        for (long outerIndex = 0; outerIndex < junctionBoxes.Length; ++outerIndex)
+        for (int outerIndex = 0; outerIndex < JunctionBoxes.Length; ++outerIndex)
         {
-            closestDistance = long.MaxValue;
-            for (long innerIndex = 0; innerIndex < junctionBoxes.Length; ++innerIndex)
+            for (int innerIndex = 0; innerIndex < JunctionBoxes.Length; ++innerIndex)
             {
                 if (outerIndex == innerIndex)
                 {
                     continue;
                 }
-                Vector3 outerVector = junctionBoxes[outerIndex];
-                Vector3 innerVector = junctionBoxes[innerIndex];
-                long distance = (long)Vector3.Distance(outerVector, innerVector);
-                if (distance < closestDistance)
-                {
-                    closestDistance = (long)distance;
-                    distanceToClosestJunctionArray[outerIndex] = distance;
-                    distanceToClosestJunctionArray[innerIndex] = distance;
-                    closestJunctionIndexArray[outerIndex] = innerIndex;
-                    closestJunctionIndexArray[innerIndex] = outerIndex;
-                }
+                
+                JunctionBox outerBox = JunctionBoxes[outerIndex];
+                JunctionBox innerBox = JunctionBoxes[innerIndex];
+                JunctionPair pairDistance = new(outerIndex, innerIndex, outerBox.Distance(innerBox));
+                JunctionDistanceList.Add(pairDistance);
             }
+        }
+        
+        // Remove entries that are duplicate pairs
+        JunctionDistanceList = JunctionDistanceList.Distinct(new JunctionPairComparer()).ToList();
+        JunctionDistanceList.Sort(new JunctionPairSorter());
+        
+        return ClosestJunctionIndexArray;
+    }
+    
+    public JunctionPair FindClosestPair(List<int> junctionBoxIndices)
+    {
+        // TODO: The thing that's wrong here is that I need to take out DIRECTLY CONNECTED pairs when considering distance.
+        // So, if 0/19 are a directly connected pair (they have each other as closest?), then no
+        //   other boxes should consider them as closest -- other boxes are looking for their closest pair?
+        // 
 
-            if (distanceToJunctionsList.ContainsKey(closestDistance))
+        JunctionPair closestPair = new();
+        closestPair.DistanceFromReferenceToClosestJunction = double.MaxValue;
+
+        foreach (int outerJunctionBoxIndex in junctionBoxIndices)
+        {
+            foreach (int innerJunctionBoxIndex in junctionBoxIndices)
             {
-                // Make sure it's just the same tuple. Really should find a way to skip it but whatever
-                if (distanceToJunctionsList[closestDistance].Item1 == outerIndex || distanceToJunctionsList[closestDistance].Item2 == outerIndex)
+                if (outerJunctionBoxIndex == innerJunctionBoxIndex)
                 {
                     continue;
                 }
-                else
+                JunctionBox outerBox = JunctionBoxes[outerJunctionBoxIndex];
+                JunctionBox innerBox = JunctionBoxes[innerJunctionBoxIndex];
+                double distance = outerBox.Distance(innerBox);
+                if (distance < closestPair.DistanceFromReferenceToClosestJunction)
                 {
-                    // panic
-                    throw new Exception("Duplicate distance");
+                    closestPair.DistanceFromReferenceToClosestJunction = distance;
+                    closestPair.ReferenceJunctionIndex = outerJunctionBoxIndex;
+                    closestPair.ClosestJunctionIndex   = innerJunctionBoxIndex;
                 }
             }
-            distanceToJunctionsList.Add(closestDistance, Tuple.Create(outerIndex, closestJunctionIndexArray[outerIndex]));
         }
         
-        return closestJunctionIndexArray;
+        return closestPair;
     }
 
-    public ArrayList ConnectClosestJunctions(long iterations)
+    public List<List<int>> ConnectClosestJunctions(long iterations, bool printCircuits = false)
     {
-        ArrayList circuits = new ArrayList();
+        List<List<int>> circuits = new List<List<int>>();
+
         // Start with every junction box in its own circut
-        for (long index = 0; index < junctionBoxes.Length; index++)
+        for (int index = 0; index < JunctionBoxes.Length; index++)
         {
-            circuits.Add(new List<long> { index });
+            circuits.Add(new List<int> { index });
         }
         
-        // TODO: start from the last index into distanceToJunctionsList we added something to
-        for (int index = 0; index < distanceToJunctionsList.Count && iterations > 0; ++index)
+        for (int index = 0; index < JunctionDistanceList.Count && iterations > 0; ++index)
         {
-            Tuple<long, long> junctionPair = distanceToJunctionsList.GetValueAtIndex(index);
-            Pair junctionToCircuitPair = new Pair(-1, -1);
-
-            int circuitIndex = 0;
-            foreach (List<long> circuit in circuits)
-            {
-                if (circuit.Contains(junctionPair.Item1))
-                {
-                    junctionToCircuitPair.Item1 = circuitIndex;
-                }
-                if (circuit.Contains(junctionPair.Item2))
-                {
-                    junctionToCircuitPair.Item2 = circuitIndex;
-                }
-
-                ++circuitIndex;
-            }
-
-            if (junctionToCircuitPair.Item1 == -1 || junctionToCircuitPair.Item2 == -1)
-            {
-                // panic again
-                throw new Exception("Junction not in any circut");
-            }
+            JunctionPair junctionPair = JunctionDistanceList[index];
             
-            if (junctionToCircuitPair.Item1 == junctionToCircuitPair.Item2)
-            {
-                // These junctions are already connected
-                --iterations;
-            }
-            
-            else if (((List<long>)circuits[junctionToCircuitPair.Item1]).Count < ((List<long>)circuits[junctionToCircuitPair.Item2]).Count)
-            {
-                ((List<long>)circuits[junctionToCircuitPair.Item2]).AddRange(((List<long>)circuits[junctionToCircuitPair.Item1]));
-                circuits.RemoveAt(junctionToCircuitPair.Item1);
-                --iterations;
-            }
-            else
-            {
-                ((List<long>)circuits[junctionToCircuitPair.Item1]).AddRange(((List<long>)circuits[junctionToCircuitPair.Item2]));
-                circuits.RemoveAt(junctionToCircuitPair.Item2);
-                --iterations;
-            }
+            ConnectJunctionPairInCicuits(circuits, junctionPair, printCircuits);
+            --iterations;
         }
         return circuits;
+    }
+
+    public void ConnectJunctionPairInCicuits(List<List<int>> circuits, JunctionPair junctionPair, bool printCircuits)
+    {
+        int circuitIndex = 0;
+        Pair junctionToCircuitPair = new Pair(-1, -1);
+            
+        foreach (List<int> circuit in circuits)
+        {
+            if (circuit.Contains(junctionPair.ReferenceJunctionIndex))
+            {
+                junctionToCircuitPair.Item1 = circuitIndex;
+            }
+            if (circuit.Contains(junctionPair.ClosestJunctionIndex))
+            {
+                junctionToCircuitPair.Item2 = circuitIndex;
+            }
+
+            ++circuitIndex;
+        }
+
+        if (junctionToCircuitPair.Item1 != -1 && (junctionToCircuitPair.Item1 == junctionToCircuitPair.Item2))
+        {
+            // These junctions are already connected
+            //--iterations;
+            throw new Exception("Tried to connect already connected junctions");
+        }
+            
+        else if (junctionToCircuitPair.Item1 == -1 || junctionToCircuitPair.Item2 == -1)
+        {
+            // It's a new circuit!
+             circuits.Add(new List<int> { junctionPair.ReferenceJunctionIndex, junctionPair.ClosestJunctionIndex });
+        }
+        else if (((List<int>)circuits[junctionToCircuitPair.Item1]).Count < ((List<int>)circuits[junctionToCircuitPair.Item2]).Count)
+        {
+            ((List<int>)circuits[junctionToCircuitPair.Item2]).AddRange(((List<int>)circuits[junctionToCircuitPair.Item1]));
+            circuits.RemoveAt(junctionToCircuitPair.Item1);
+            if (printCircuits)
+            {
+                PrintCircuits(circuits);
+            }
+        }
+        else
+        {
+            ((List<int>)circuits[junctionToCircuitPair.Item1]).AddRange(((List<int>)circuits[junctionToCircuitPair.Item2]));
+            circuits.RemoveAt(junctionToCircuitPair.Item2);
+            if (printCircuits)
+            {
+                PrintCircuits(circuits);
+            }
+        }
+    }
+
+    public List<List<int>> CreateCircuitsList(long iterations, bool printCircuits = false)
+    {
+        // Maybe the next algorithm is really:
+        //  1. Start with an EMPTY circuit list
+        //  2. Find the closest distance pair. Connect them. Remove them from consideration for finding a closest pair.
+        //      (but they could still the the closest box to another unconnected box)
+        //  3. For every unconnected box, find the closest distance pair.
+        List<List<int>> circuits = new List<List<int>>();
+        List<int> validJunctionIndices = Enumerable.Range(0, JunctionBoxes.Length).ToList(); 
+        
+        while (iterations > 0)
+        {
+            JunctionPair closestPair = FindClosestPair(validJunctionIndices);
+            ConnectJunctionPairInCicuits(circuits, closestPair, printCircuits);
+            // validJunctionIndices.Remove(closestPair.ReferenceJunctionIndex);
+            // validJunctionIndices.Remove(closestPair.ClosestJunctionIndex);
+            --iterations;
+        }
+
+        return circuits;
+    }
+    
+    public long MultiplyLarestCircuits(List<List<int>> circuits, int numberOfCircuitsToMultiply)
+    {
+        circuits.Sort(new CircuitListSorter());
+        long multiple = 1;
+        for (int index = 0; index < numberOfCircuitsToMultiply; ++index)
+        {
+            multiple *= circuits[index].Count;
+        }
+        return multiple;
     }
     
     public void Part1()
--- AoC2025/Tests/Day8Tests.cs
+++ AoC2025/Tests/Day8Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Numerics;
 using NUnit.Framework;
 namespace AoC2025.Tests;
 
@@ -12,7 +13,7 @@ public class Day8Tests
         var lines = InputReader.GetInputLines("../../../Inputs/Day8TestInput.txt");
         Day8 day8 = new Day8();
         day8.ParseLines(lines);
-        Assert.That(day8.junctionBoxes.Length, Is.EqualTo(20));
+        Assert.That(day8.JunctionBoxes.Length, Is.EqualTo(20));
         day8.PrintJunctionBoxes();
     }
 
@@ -22,12 +23,42 @@ public class Day8Tests
         var lines = InputReader.GetInputLines("../../../Inputs/Day8TestInput.txt");
         Day8 day8 = new Day8();
         day8.ParseLines(lines);
-        long[] closestJunctionIndexArray = day8.CalculateDistancesBetweenJunctionBoxes();
+        int[] closestJunctionIndexArray = day8.CalculateDistancesBetweenJunctionBoxes();
         Assert.That(closestJunctionIndexArray.Length, Is.EqualTo(20));
         Assert.That(closestJunctionIndexArray[0], Is.EqualTo(19));
         Assert.That(closestJunctionIndexArray[19], Is.EqualTo(0));
+//        Assert.That(closestJunctionIndexArray[7], Is.EqualTo(19));
     }
 
+    [Test]
+    public void TestJunctionPairSorterSortsSmallestDistancesFirst()
+    {
+        Day8 day8 = new Day8();
+        day8.JunctionBoxes = new JunctionBox[3];
+        day8.JunctionBoxes[0] = new JunctionBox(0, 0, 0);
+        day8.JunctionBoxes[1] = new JunctionBox(1, 0, 0);
+        day8.JunctionBoxes[2] = new JunctionBox(4, 0, 0);
+        
+        day8.CalculateDistancesBetweenJunctionBoxes();
+        day8.JunctionDistanceList.Sort(new JunctionPairSorter());
+        
+        Assert.That(day8.JunctionDistanceList[0].DistanceFromReferenceToClosestJunction,
+            Is.LessThan(day8.JunctionDistanceList[2].DistanceFromReferenceToClosestJunction));
+    }
+
+    [Test]
+    public void TestCircuitListSorterSortsLargestGroupsFirst()
+    {
+        List<int> circuit1 = new List<int> { 1, 2, 3 };
+        List<int> circuit2 = new List<int> { 4, 5 };
+        List<int> circuit3 = new List<int> { 6 };
+        
+        List<List<int>> circuits = new List<List<int>> { circuit3, circuit3, circuit2, circuit1 };
+        circuits.Sort(new CircuitListSorter());
+        
+        Assert.That(circuits[0].Count, Is.EqualTo(3));
+    }
+    
     [Test]
     public void TestCanCombine1Circuit()
     {
@@ -35,9 +66,8 @@ public class Day8Tests
         Day8 day8 = new Day8();
         day8.ParseLines(lines);
         day8.CalculateDistancesBetweenJunctionBoxes();
-        ArrayList circuits = day8.ConnectClosestJunctions(1);
+        List<List<int>> circuits = day8.ConnectClosestJunctions(1);
         Assert.That(circuits.Count, Is.EqualTo(19));
-        
     }
     
     [Test]
@@ -48,7 +78,7 @@ public class Day8Tests
         day8.ParseLines(lines);
         day8.CalculateDistancesBetweenJunctionBoxes();
         
-        ArrayList circuits = day8.ConnectClosestJunctions(2);
+        List<List<int>> circuits = day8.ConnectClosestJunctions(2);
         Assert.That(circuits.Count, Is.EqualTo(18));
     }
 
@@ -60,7 +90,7 @@ public class Day8Tests
         day8.ParseLines(lines);
         day8.CalculateDistancesBetweenJunctionBoxes();
         
-        ArrayList circuits = day8.ConnectClosestJunctions(3);
+        List<List<int>> circuits = day8.ConnectClosestJunctions(3);
         Assert.That(circuits.Count, Is.EqualTo(17));
     }
 
@@ -73,7 +103,44 @@ public class Day8Tests
         day8.ParseLines(lines);
         day8.CalculateDistancesBetweenJunctionBoxes();
         
-        ArrayList circuits = day8.ConnectClosestJunctions(10);
+        List<List<int>> circuits = day8.ConnectClosestJunctions(10, true);
+        day8.PrintCircuits(circuits);
+
         Assert.That(circuits.Count, Is.EqualTo(11));
+    }
+
+    [Test]
+    public void TestCanMultiplyTest10Circuits()
+    {
+        var lines = InputReader.GetInputLines("../../../Inputs/Day8TestInput.txt");
+        Day8 day8 = new Day8();
+        day8.ParseLines(lines);
+        day8.CalculateDistancesBetweenJunctionBoxes();
+        
+        List<List<int>> circuits = day8.ConnectClosestJunctions(10, true);
+        Assert.That(day8.MultiplyLarestCircuits(circuits, 3), Is.EqualTo(40));
+    }
+
+    [Test]
+    public void TestCanCombineOneCircuit_CreateCircuitsList()
+    {
+        var lines = InputReader.GetInputLines("../../../Inputs/Day8TestInput.txt");
+        Day8 day8 = new Day8();
+        day8.ParseLines(lines);
+        List<List<int>> circuits = day8.CreateCircuitsList(1, true);
+        Assert.That(circuits.Count, Is.EqualTo(1));
+        Assert.That(circuits[0].Count, Is.EqualTo(2));
+    }
+    
+    [Test]
+    public void TestCanCombineTwoCircuit_CreateCircuitsList()
+    {
+        var lines = InputReader.GetInputLines("../../../Inputs/Day8TestInput.txt");
+        Day8 day8 = new Day8();
+        day8.ParseLines(lines);
+        List<List<int>> circuits = day8.CreateCircuitsList(2, true);
+        day8.PrintCircuits(circuits);
+        Assert.That(circuits.Count, Is.EqualTo(1));
+        Assert.That(circuits[0].Count, Is.EqualTo(3));
     }
 }
